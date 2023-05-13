5598e5b186f9ac4085fa4cc1a0d9da48703e55fb
Araya Alejo <82895574+Shacaso@users.noreply.github.com>
2023-05-13 14:31:53 -0300

    Create ComboBoxModel.tsx

---
+++ src/components/modals/_ComboBoxModel/ComboBoxModel.tsx
@@ -0,0 +1,29 @@
+import { Field } from "formik"
+
+interface Props {
+    list: any[],
+    name: string,
+    title: string,
+    value: string
+}
+
+const ComboBoxModel = ({ list, name, title, value }: Props) => {
+    return (
+
+        <>
+            <label htmlFor={name}>{title}</label>
+            <Field name={name} as="select">
+                <option value="">Select {value}</option>
+                {
+                    list.map((e, index) => (
+                        <option value={JSON.stringify(e)} key={index}>{e.name}</option>
+                    ))}
+            </Field>
+        </>
+    )
+
+}
+
+
+
+export default ComboBoxModel
