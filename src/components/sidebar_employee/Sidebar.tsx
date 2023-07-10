eff8efcbe3ee21afa8b050eb31d982588b303406
Araya Alejo <82895574+Shacaso@users.noreply.github.com>
2023-07-10 16:02:33 -0300

    refactor sidebar

--- src/components/sidebar_employee/Sidebar.tsx
+++ src/components/sidebar_employee/Sidebar.tsx
@@ -1,53 +1,108 @@
 import './sidebar.scss'
 import { useAuth0 } from '@auth0/auth0-react'
-import { useNavigate } from 'react-router-dom'
-import { RiUser4Fill } from 'react-icons/ri'
-import { TbCircleFilled } from 'react-icons/tb'
+import { NavLink, Route, Routes, useNavigate } from 'react-router-dom'
+import loguot_icon from '../../assets/logout_icon.svg'
 
 function Sidebar() {
   const { user, logout } = useAuth0()
   const navigate = useNavigate()
   return (
-    <div className="container">
-      <div className="container-img-user-role">
-        <div className="left-profile-img-container">
-          <TbCircleFilled className="left-circle-img"/>
-          <RiUser4Fill className="left-profile-img" />
-        </div>
-        <span className="left-username">{`${user?.given_name} ${user?.family_name}`}</span>
-        <span className="left-role">Admin</span>
-      </div>
-      <div className="nav-container">
-        <div className="dropdown">
-          <button className="dropbtn">CRUD</button>
-          <div className="dropdown-content">
-            <ul className="nav-list">
-              <li className="nav-list-item" onClick={() => navigate('/')}>Home</li>
-              <li className="nav-list-item" onClick={() => navigate('./categories')}>Categories</li>
-              <li className="nav-list-item" onClick={() => navigate('./measures')}>Measures</li>
-              <li className="nav-list-item" onClick={() => navigate('./ingredients')}>Ingredient</li>
-              <li className="nav-list-item" onClick={() => navigate('./products')}>Products</li>
-              <li className="nav-list-item" onClick={() => navigate('./orders')}>Orders</li>
-              <li className="nav-list-item" onClick={() => navigate('./bills')}>Bills</li>
-              <li className="nav-list-item" onClick={() => navigate('./users')}>Users</li>
-              <li className="nav-list-item" onClick={() => navigate('./sections')}>Sections</li>
-              <li className="nav-list-item" onClick={() => navigate('./locations')}>Locations</li>
-              <li className="nav-list-item" onClick={() => navigate('./addresses')}>Addresses</li>
-              {/* <li className="nav-list-item">My Profile</li>
-                <li className="nav-list-item">Adressess</li>
-                <li className="nav-list-item">Order History</li>
-                <li className="nav-list-item">Users</li>
-                <li className="nav-list-item">Products</li>
-                <li className="nav-list-item">Rankings</li>
-                <li className="nav-list-item">Movements</li> */}
-            </ul>
+
+    <div className='grid grid-cols-[260px_1fr] h-[100vh] overflow-x-hidden overflow-y-auto gap-3'>
+
+      <ul className="w-56 m-4 menu bg-secondary rounded-box">
+        <div className="flex justify-center avatar">
+          <div className="w-24 rounded-full">
+            <img src={user?.picture} />
           </div>
         </div>
-      </div>
-      <div className="nav_container">
-        <ul>
-          <li className="nav-list-item-logout" onClick={() => logout()}>Log Out</li>
-        </ul>
+        <h3 className='text-center text-gray-600 menu-title'>{user?.given_name + ' ' + user?.family_name || "User"}</h3>
+        <div className='flex flex-col justify-between h-[75%]'>
+          <div>
+            <li>
+              <h2 className='menu-title '>Home</h2>
+            </li>
+            <li>
+              <h2 className='menu-title '>My profile</h2>
+            </li>
+            <li>
+              <details open>
+                <summary>
+                  <h2 className="p-0 text-left menu-title">Tables</h2>
+
+                </summary>
+                <ul>
+                  <li>
+                    <NavLink to="categories"
+                      end
+                      className={({ isActive }) => isActive ? "active" : ""}>categories</NavLink>
+                  </li>
+                  <li>
+                    <NavLink to="measures"
+                      className={({ isActive }) => isActive ? "active" : ""}>measures</NavLink></li>
+                  <li>
+                    <NavLink to="ingredients"
+                      className={({ isActive }) => isActive ? "active" : ""}>ingredients</NavLink></li>
+                  <li>
+                    <NavLink to="products"
+                      className={({ isActive }) => isActive ? "active" : ""}>products</NavLink></li>
+                  <li>
+                    <NavLink to="orders"
+                      className={({ isActive }) => isActive ? "active" : ""}>orders</NavLink></li>
+                  <li>
+                    <NavLink to="bills"
+                      className={({ isActive }) => isActive ? "active" : ""}>bills</NavLink></li>
+                  <li>
+                    <NavLink to="users"
+                      className={({ isActive }) => isActive ? "active" : ""}>users</NavLink></li>
+                  <li>
+                    <NavLink to="sections"
+                      className={({ isActive }) => isActive ? "active" : ""}>sections</NavLink></li>
+                  <li>
+                    <NavLink to="locations"
+                      className={({ isActive }) => isActive ? "active" : ""}>locations</NavLink></li>
+                  <li>
+                    <NavLink to="addresses"
+                      className={({ isActive }) => isActive ? "active" : ""}>addresses</NavLink></li>
+                </ul>
+              </details>
+            </li>
+            <li>
+              <h2 className='menu-title '>Stock</h2>
+            </li>
+            <li>
+              <h2 className='menu-title '>Rankings</h2>
+            </li>
+            <li>
+              <h2 className='menu-title '>Movements</h2>
+            </li>
+
+          </div>
+          <ul>
+            <li className="nav-list-item-logout" onClick={() => logout()}>
+              <div className='flex justify-center'>
+                <h2>Log Out</h2>
+                <img className='h-3' src={loguot_icon} />
+              </div>
+            </li>
+          </ul>
+        </div>
+        {/* <li>
+          <h2 className="menu-title">Orders</h2>
+          <ul>
+            <li><NavLink to="orders"
+              className={({ isActive }) => isActive ? "active" : ""}>Order history</NavLink></li>
+          </ul>
+        </li> */}
+      </ul>
+
+      <div className=''>
+        {/* <Routes>
+          <Route path="/" element={<UserDetails />} />
+          <Route path="/addresses" element={<Address />} />
+          <Route path="/password-form" element={<Change_password />} />
+          <Route path="/orders" element={<History_Order />} />
+        </Routes> */}
       </div>
     </div>
 
