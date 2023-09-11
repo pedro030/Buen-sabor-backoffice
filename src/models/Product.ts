e4d325a2b41cf9f722a7e36b63f204a4a98350d4
Araya Alejo <82895574+Shacaso@users.noreply.github.com>
2023-09-11 13:18:46 -0300

    product ranking

--- src/models/Product.ts
+++ src/models/Product.ts
@@ -1,9 +1,14 @@
 import { Category } from "./Category";
+import { Ingredient } from "./Ingredient";
 
 export interface Product{
     id: string,
     name: string,
+    image: string,
     price: number,
+    cookingTime: number,
+    quantitySold: number,
     subcategory: Category
+    ingredients: Array<Ingredient>
     active: boolean
 }
--- src/pages/Rankings/RankingsProducts/RankingsProducts.tsx
+++ src/pages/Rankings/RankingsProducts/RankingsProducts.tsx
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react"
 import store from "../../../state/store/store";
+import { Product } from "../../../models/Product";
+import { RiFileExcel2Line } from 'react-icons/ri';
 
 const RankingsProducts = () => {
   const token = store.getState().userSession.token
   const apiURL = import.meta.env.VITE_REACT_APP_API_URL;
-  const [products, setProducts] = useState([])
+  const [products, setProducts] = useState<Array<Product>>([])
 
   useEffect(() => {
     fetch(`${apiURL}/products/getByQuanSold`, {
@@ -18,15 +20,97 @@ const RankingsProducts = () => {
   }, [])
 
   return (
-    <div>
-      Rankings Products
-      {
-        products.map((product: any, index: number) => {
-          <div key={index}>
-            <h1>{product.name}</h1>
+    <div className="h-[100vh] overflow-y-auto">
+      <h1 className="my-5 text-xl font-semibold tracking-widest text-center uppercase">Rankings Products</h1>
+      <hr />
+      <div>
+        <h1 className="mt-5 text-lg tracking-widest text-center ">COCINA</h1>
+        <div className="flex justify-center gap-5 my-2">
+          <div>
+            <label className="label">
+              <span className="text-gray-500 label-text">Desde</span>
+            </label>
+            <input className="input input-sm input-bordered" type="date" />
           </div>
-        })
-      }
+          <div>
+            <label className="label">
+              <span className="text-gray-500 label-text">Hasta</span>
+            </label>
+            <input className="input input-sm input-bordered" type="date" />
+          </div>
+        </div>
+        <table className="table table-xs">
+          <thead>
+            <tr>
+              <th>NAME</th>
+              <th className="text-center">QUANTITY SOLD</th>
+              <th>ACTIVE</th>
+              <th className="text-center">PRICE</th>
+              <th>CATEGORY</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              products.filter((item: Product) => item.subcategory?.name !== "Gaseosas").map((product: Product, index: number) => (
+                <tr key={index}>
+                  <th >{product.name}</th>
+                  <th className="text-center">{product.quantitySold}</th>
+                  <th ><div className={`${product.active ? 'badge badge-success text-white' : 'badge badge-primary'}`}>{product.active ? "Active" : "No Active"}</div></th>
+                  <th className="text-center">{product.price}</th>
+                  <th >{product.subcategory?.name}</th>
+                </tr>
+              ))
+            }
+          </tbody>
+        </table>
+      </div>
+
+      <div>
+        <h1 className="mt-10 text-lg tracking-widest text-center ">BEBIDAS</h1>
+        <div className="flex justify-center gap-5 my-2">
+          <div>
+            <label className="label">
+              <span className="text-gray-500 label-text">Desde</span>
+            </label>
+            <input className="input input-sm input-bordered" type="date" />
+          </div>
+          <div>
+            <label className="label">
+              <span className="text-gray-500 label-text">Hasta</span>
+            </label>
+            <input className="input input-sm input-bordered" type="date" />
+          </div>
+        </div>
+        <table className="table table-xs">
+          <thead>
+            <tr>
+            <th>NAME</th>
+              <th className="text-center">QUANTITY SOLD</th>
+              <th>ACTIVE</th>
+              <th className="text-center">PRICE</th>
+              <th>CATEGORY</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              products.filter((item: Product) => item.subcategory?.name === "Gaseosas").map((product: Product, index: number) => (
+                <tr key={index}>
+                <th >{product.name}</th>
+                <th className="text-center">{product.quantitySold}</th>
+                <th ><div className={`${product.active ? 'badge badge-success text-white' : 'badge badge-primary'}`}>{product.active ? "Active" : "No Active"}</div></th>
+                <th className="text-center">{product.price}</th>
+                <th >{product.subcategory?.name}</th>
+              </tr>
+              ))
+            }
+          </tbody>
+        </table>
+      </div>
+
+      <div className="flex justify-end w-full mt-10 mb-5 ">
+        <button className="mr-10 text-white btn btn-success btn-wide"><RiFileExcel2Line />EXPORT EXCEL</button>
+      </div>
+
     </div>
   )
 }
