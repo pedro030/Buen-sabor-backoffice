136cc453bc06e057420c5692c82a74cb2bd2ea7d
Asiel <asiel.alvarez@alumnos.frm.utn.edu.ar>
2023-09-21 11:35:12 -0300

    Product Ranking Update

--- src/pages/Rankings/RankingsProducts/RankingsProducts.tsx
+++ src/pages/Rankings/RankingsProducts/RankingsProducts.tsx
@@ -2,11 +2,36 @@ import { useEffect, useState } from "react"
 import store from "../../../state/store/store";
 import { Product } from "../../../models/Product";
 import { RiFileExcel2Line } from 'react-icons/ri';
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 const RankingsProducts = () => {
   const token = store.getState().userSession.token
   const apiURL = import.meta.env.VITE_REACT_APP_API_URL;
   const [products, setProducts] = useState<Array<Product>>([])
+  const [initialRanking, setInitialRanking] = useState<Array<Product>>([])
+  const [foodRanking, setFoodRanking] = useState<boolean>(true)
+  const [startDate, setStartDate] = useState<Date | null>(null);
+  const [endDate, setEndDate] = useState<Date | null>(null);
+
+  // Format Date. Example: 2023-6-2 to 2023-06-02
+  const formatToConsistentDate = (inputDate: string) => {
+    const parts = inputDate.split("-");
+    const year = parseInt(parts[0]);
+    const month = parseInt(parts[1].length === 1 ? `0${parts[1]}` : parts[1]) - 1;
+    const day = parseInt(parts[2].length === 1 ? `0${parts[2]}` : parts[2]);
+    
+    return new Date(year, month, day);
+  }
+
+  // Handle Change DatePicker
+  const handleChangeDate = (range: [Date, Date]) => {
+    const [startDate, endDate] = range;
+    setStartDate(startDate);
+    setEndDate(endDate);
+
+    if(!startDate && !endDate) setProducts(initialRanking);
+  };
 
   useEffect(() => {
     fetch(`${apiURL}/products/getByQuanSold`, {
@@ -15,41 +40,55 @@ const RankingsProducts = () => {
       }
     })
       .then(res => res.json())
-      .then(data => setProducts(data))
+      .then(data => {setProducts(data); setInitialRanking(data)})
       .catch(error => console.error(error))
   }, [])
 
   return (
     <div className="h-[100vh] overflow-y-auto">
-      <h1 className="my-5 text-xl font-semibold tracking-widest text-center uppercase">Rankings Products</h1>
+      <h1 className="my-5 text-xl font-semibold tracking-widest text-center uppercase">Product Ranking</h1>
       <hr />
       <div>
-        <h1 className="mt-5 text-lg tracking-widest text-center ">COCINA</h1>
-        <div className="flex justify-center gap-5 my-2">
-          <div>
-            <label className="label">
-              <span className="text-gray-500 label-text">Desde</span>
-            </label>
-            <input className="input input-sm input-bordered" type="date" />
-          </div>
-          <div>
-            <label className="label">
-              <span className="text-gray-500 label-text">Hasta</span>
-            </label>
-            <input className="input input-sm input-bordered" type="date" />
-          </div>
+        <div className="grid grid-cols-2 join">
+          <input className="rounded-full join-item btn" type="radio" name="ranking" aria-label="FOOD RANKING" onClick={() => { setFoodRanking(true); setStartDate(null); setEndDate(null) }} defaultChecked={foodRanking ? true : false} />
+          <input className="rounded-full join-item btn" type="radio" name="ranking" aria-label="DRINKS RANKING" onClick={() => { setFoodRanking(false); setStartDate(null); setEndDate(null) }} defaultChecked={!foodRanking ? true : false} />
         </div>
-        <table className="table table-xs">
-          <thead>
-            <tr>
-              <th>NAME</th>
-              <th className="text-center">QUANTITY SOLD</th>
-              <th>ACTIVE</th>
-              <th className="text-center">PRICE</th>
-              <th>CATEGORY</th>
-            </tr>
-          </thead>
-          <tbody>
+        <h1 className="mt-5 text-lg tracking-widest text-center ">{foodRanking ? 'FOOD RANKING' : 'DRINKS RANKING'}</h1>
+          <div className="flex justify-center gap-5 my-2">
+            <div>
+                <DatePicker
+                    isClearable
+                    withPortal
+                    selectsRange
+                    selected={startDate}
+                    startDate={startDate}
+                    endDate={endDate}
+                    onChange={handleChangeDate}
+                    placeholderText="Date: From - To"
+                    dateFormat="yyyy/MM/dd"
+                    className="input input-sm input-bordered"
+                    maxDate={new Date(Date.now())}
+                  />
+            </div>
+            <div>
+              <button className="btn btn-primary btn-sm">Get Ranking by Date</button>
+            </div>
+            <div>
+              <button className="text-white btn btn-success"><RiFileExcel2Line />EXPORT EXCEL</button>
+            </div>
+          </div>
+        { foodRanking ? (<>
+          <table className="table table-xs">
+            <thead>
+              <tr>
+                <th>NAME</th>
+                <th className="text-center">QUANTITY SOLD</th>
+                <th>ACTIVE</th>
+                <th className="text-center">PRICE</th>
+                <th>CATEGORY</th>
+              </tr>
+            </thead>
+            <tbody>
             {
               products.filter((item: Product) => item.subcategory?.name !== "Gaseosas").map((product: Product, index: number) => (
                 <tr key={index}>
@@ -63,52 +102,35 @@ const RankingsProducts = () => {
             }
           </tbody>
         </table>
-      </div>
-
-      <div>
-        <h1 className="mt-10 text-lg tracking-widest text-center ">BEBIDAS</h1>
-        <div className="flex justify-center gap-5 my-2">
-          <div>
-            <label className="label">
-              <span className="text-gray-500 label-text">Desde</span>
-            </label>
-            <input className="input input-sm input-bordered" type="date" />
-          </div>
-          <div>
-            <label className="label">
-              <span className="text-gray-500 label-text">Hasta</span>
-            </label>
-            <input className="input input-sm input-bordered" type="date" />
-          </div>
-        </div>
-        <table className="table table-xs">
-          <thead>
-            <tr>
-            <th>NAME</th>
-              <th className="text-center">QUANTITY SOLD</th>
-              <th>ACTIVE</th>
-              <th className="text-center">PRICE</th>
-              <th>CATEGORY</th>
-            </tr>
-          </thead>
-          <tbody>
-            {
-              products.filter((item: Product) => item.subcategory?.name === "Gaseosas").map((product: Product, index: number) => (
-                <tr key={index}>
-                <th >{product.name}</th>
-                <th className="text-center">{product.quantitySold}</th>
-                <th ><div className={`${product.active ? 'badge badge-success text-white' : 'badge badge-primary'}`}>{product.active ? "Active" : "No Active"}</div></th>
-                <th className="text-center">{product.price}</th>
-                <th >{product.subcategory?.name}</th>
+        </>)
+        : 
+        (<>
+          <table className="table table-xs">
+            <thead>
+              <tr>
+              <th>NAME</th>
+                <th className="text-center">QUANTITY SOLD</th>
+                <th>ACTIVE</th>
+                <th className="text-center">PRICE</th>
+                <th>CATEGORY</th>
               </tr>
-              ))
-            }
-          </tbody>
-        </table>
-      </div>
-
-      <div className="flex justify-end w-full mt-10 mb-5 ">
-        <button className="mr-10 text-white btn btn-success btn-wide"><RiFileExcel2Line />EXPORT EXCEL</button>
+            </thead>
+            <tbody>
+              {
+                products.filter((item: Product) => item.subcategory?.name === "Gaseosas").map((product: Product, index: number) => (
+                  <tr key={index}>
+                  <th >{product.name}</th>
+                  <th className="text-center">{product.quantitySold}</th>
+                  <th ><div className={`${product.active ? 'badge badge-success text-white' : 'badge badge-primary'}`}>{product.active ? "Active" : "No Active"}</div></th>
+                  <th className="text-center">{product.price}</th>
+                  <th >{product.subcategory?.name}</th>
+                </tr>
+                ))
+              }
+            </tbody>
+          </table>
+        </>)}
+        
       </div>
 
     </div>
